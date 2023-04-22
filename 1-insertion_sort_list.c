9ba002109107f0eabdc862eb5b5ad03246341834
Ayom04 <abdullahayomide04@gmail.com>
2023-04-22 14:19:52 +0100

    updating insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,32 +1,36 @@
-include "sort.h"
+#include "sort.h"
 
 /**
- * swap - Swap nodes
- * @h: Head pointer to the linked list
- * @sort_l: pointer to the sorted part of linked list
- * @temp: Pointer to node whise value is being compared
- * Return: Nothing
+ *swap - Swap nodes
+ *@h: Head pointer to the linked list
+ *@sort_l: pointer to the sorted part of linked list
+ *@temp: Pointer to node whise value is being compared
+ *Return: Nothing
  */
 
 void swap(listint_t **h, listint_t **sort_l, listint_t *temp)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	if  (temp->prev)
+		temp->prev->next = temp->next;
+
+	if ((*sort_l)->prev)
+		(*sort_l)->prev->next = temp;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*h = temp;
+	temp->prev = (*sort_l)->prev;
+	(*sort_l)->prev = temp;
+	temp->next = *sort_l;
+	*sort_l = temp->prev;
 }
+
+
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * using the insertion sort algorithm.
- * @list: Array to be sorted.
- * Return: Nothing
+ *insertion_sort_list - Sorts a doubly linked list of integers
+ *using the insertion sort algorithm.
+ *@list: Array to be sorted.
+ *Return: Nothing
  */
 
 void insertion_sort_list(listint_t **list)
@@ -42,6 +46,7 @@ void insertion_sort_list(listint_t **list)
 	{
 		temp = h;
 		sort_l = h->prev;
+
 		while (sort_l != NULL && sort_l->n > temp->n)
 		{
 			swap(list, &sort_l, temp);
